2b54f91193fc40fc074f665929d5b3f2565a51c4
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-31 16:44:42 +0200

    Made 70*.cpp computed at compile time

--- Solutions_Easy/0070_ClimbingStairs.cpp
+++ Solutions_Easy/0070_ClimbingStairs.cpp
@@ -1,8 +1,7 @@
+// Get normal solution by removing the constexpr, this is computed at compile time like this
 class Solution {
 private:
-    int memoized[45];
-
-    int _climb_stairs(int n) {
+    static constexpr int _climb_stairs(int n, int memoized[45]) {
         if (n <= 0) {
             return 0;
         }
@@ -10,13 +9,13 @@ private:
             return 1;
         }
         if (memoized[n - 1] == -1) {
-            memoized[n - 1] = _climb_stairs(n - 1) + _climb_stairs(n - 2);
+            memoized[n - 1] = _climb_stairs(n - 1, memoized) + _climb_stairs(n - 2, memoized);
         }
         return memoized[n - 1];
     }
 
 public:
-    int climbStairs(int n) {
+    static constexpr int climbStairs(int n) {
         if (n == 1) {
             return 1;
         }
@@ -27,12 +26,13 @@ public:
             return 3;
         }
 
+        int memoized[45] = {0};
         memoized[0] = 1;
         memoized[1] = 2;
         for (int i = 2; i < 45; ++i) {
             memoized[i] = -1;
         }
 
-        return _climb_stairs(n);
+        return _climb_stairs(n, memoized);
     }
 };
