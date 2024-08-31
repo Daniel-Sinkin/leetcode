c846cfa79179594ce1ff83c4c6f02aedd49f353a
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-08-31 18:21:33 +0200

    ADded 94 cpp and python solution

---
+++ Solutions_Easy/0094_BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        if (root == nullptr) {
+            return {};
+        }
+        vector<int> result;
+        vector<int> left = inorderTraversal(root->left);
+        vector<int> right = inorderTraversal(root->right);
+        result.insert(result.end(), left.begin(), left.end());
+        result.push_back(root->val);
+        result.insert(result.end(), right.begin(), right.end());
+        return result;
+    }
+};
