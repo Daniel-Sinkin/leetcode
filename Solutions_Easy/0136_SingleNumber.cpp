586a831b7b3b5420e0ed755bba748325c1defc6d
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-01 04:12:24 +0200

    Linting

--- Solutions_Easy/0136_SingleNumber.cpp
+++ Solutions_Easy/0136_SingleNumber.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    int singleNumber(vector<int> &nums) {
         // Inspired by https://www.youtube.com/watch?v=c33AZBnRHks
         // in particular this solution https://github.com/oisyn/parkerwords
         int XOR = 0;
-        for(int n : nums) {
+        for (int n : nums) {
             XOR ^= n;
         }
         return XOR;
