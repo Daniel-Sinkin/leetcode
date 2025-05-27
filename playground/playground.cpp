92a56af39681ba0ea7c5da74ad4e30459800ed50
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-05-27 02:28:58 +0200

    Removed visualizer stuff, adjusted CMakelists

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <print>
+#include <utility>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello, Playground!" << std::endl;
-    return 0;
+using std::print, std::println;
+
+using Vector = std::vector<int>;
+
+auto main() -> int {
+    size_t k = 1;
+    Vector arr = {1, 2, 3, 4, 5, 6, 7};
+
+    k %= arr.size();
+    for (size_t i = 0; i < arr.size(); ++i) {
+        std::swap(arr[i], arr[(arr.size() + i - k) % arr.size()]);
+        println("{}", arr);
+    }
+    return EXIT_SUCCESS;
 }
