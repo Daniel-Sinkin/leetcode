61e61cde2dcab53b9b27bf908aca8baa4ebac99b
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-10-13 04:51:29 +0200

    Added solution of 0036

--- Solutions_Medium/0036_ValidSodoku.cpp
+++ Solutions_Medium/0036_ValidSodoku.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        uint64_t encoding = 0;
-        uint64_t currDigit = 0;
+    bool isValidSudoku(vector<vector<char>> &board) {
+        std::array<std::bitset<9>, 9> row_bits{};
+        std::array<std::bitset<9>, 9> col_bits{};
+        std::array<std::bitset<9>, 9> block_bits{};
 
-        // Rows
-        for(int r = 0; r < 9; r++) {
-            for(int i = 0; i < 9; i++) {
-                if(board[r][i] != '.') {
-                    currDigit = 1 << (int(board[r][i]) - 48);
-                    if((encoding & currDigit) != 0) {
-                        return false;
-                    }
-                    else
-                        encoding = encoding + (1 << (int(board[r][i]) - 48));
+        for (size_t row = 0; row < 9; ++row) {
+            for (size_t col = 0; col < 9; ++col) {
+                char c = board[row][col];
+                if (c == '.') continue;
+
+                int bit = c - '1';
+                size_t block = (row / 3) * 3 + (col / 3);
+
+                if (row_bits[row].test(bit) ||
+                    col_bits[col].test(bit) ||
+                    block_bits[block].test(bit)) {
+                    return false;
                 }
-            }
-            encoding = 0;
-        }
 
-        // Columns
-        for(int i = 0; i < 9; i++) {
-            for(int r = 0; r < 9; r++) {
-                if(board[r][i] != '.') {
-                    currDigit = 1 << (int(board[r][i]) - 48);
-                    if((encoding & currDigit) != 0) {
-                        return false;
-                    }
-                    else
-                        encoding = encoding + currDigit;
-                }
+                row_bits[row].set(bit);
+                col_bits[col].set(bit);
+                block_bits[block].set(bit);
             }
-            encoding = 0;
         }
-
-        // 00 01 02 10 11 12 20 21 22
-        for(int row = 0; row < 3; row++) {
-            for(int col = 0; col < 3; col++) {
-                for(int r = 0; r < 3; r++) {
-                    for(int c = 0; c < 3; c++) {
-                        if(board[r + 3 * row][c + 3 * col] != '.') {
-                            currDigit = 1 << (int(board[r + 3 * row][c + 3 * col]) - 48);
-                            cout << (bitset<16>(currDigit)).to_string() << endl;
-                            if((encoding & currDigit) != 0) {
-                                cout << "Broke here!" << endl;
-                                return false;
-                            }
-                            else
-                                encoding = encoding + currDigit;
-                        }
-                    }
-                }
-                encoding = 0;
-            }
-            encoding = 0;
-        }
-
         return true;
     }
 };
--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 #include <print>
 #include <string>
@@ -7,9 +8,61 @@
 using std::print, std::println;
 
 auto main() -> int {
-    std::string s = "hellomynameisdaniel";
+    using std::vector;
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
-    for (const char &s_char : s) {
-        auto x = s_char - 'a';
+    std::array<std::bitset<9>, 9> row_bits{};
+    std::array<std::bitset<9>, 9> col_bits{};
+    std::array<std::bitset<9>, 9> block_bits{};
+
+    for (size_t row = 0; row < 9; ++row) {
+        for (size_t col = 0; col < 9; ++col) {
+            char c = board[row][col];
+            if (c == '.') continue;
+
+            int bit = c - '1';
+            size_t block = (row / 3) * 3 + (col / 3);
+
+            if (row_bits[row].test(bit) ||
+                col_bits[col].test(bit) ||
+                block_bits[block].test(bit)) {
+                return false;
+            }
+
+            row_bits[row].set(bit);
+            col_bits[col].set(bit);
+            block_bits[block].set(bit);
+        }
+    }
+
+    for (const vector<char> &row : board) {
+        std::bitset<9> row_pattern{0b0000'0000};
+        for (char entry : row) {
+            if (entry != '.') {
+                int idx = entry - '1';
+                if (row_pattern.test(idx)) return false;
+                row_pattern.set(idx);
+            }
+        }
+    }
+    for (size_t col_idx = 0; col_idx < 9; ++col_idx) {
+        std::bitset<9> col_pattern1{0b0000'0000};
+        std::bitset<9> col_pattern2{0b0000'0000};
+        std::bitset<9> col_pattern3{0b0000'0000};
+        std::bitset<9> col_pattern4{0b0000'0000};
+        for (size_t i = 0; i < 9; ++i) {
+            vector<char> board[i];
+
+            char c1 = board[i] board[col_idx]
+        }
     }
 }
